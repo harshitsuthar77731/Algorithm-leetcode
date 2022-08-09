8decc0955ac6768216a6089219cdacbbe7386826
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-08-09 10:49:06 +0530

    Create 70. Climbing Stairs.cpp

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int ans = 0;
+    int dp[46];
+    int fun(int n){
+        if(n==0)
+            return 1;
+        if(n<0)
+            return 0;
+        if(dp[n]!=-1)
+            return dp[n];
+        return dp[n] = fun(n-1)+fun(n-2);
+    }
+    int climbStairs(int n) {
+        memset(dp,-1,sizeof(dp));
+        return fun(n);
+    }
+};
