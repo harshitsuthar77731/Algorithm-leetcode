83882595ea7b7119287bebe329127cb8bfdbbf8d
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-08-10 17:33:11 +0530

    Create 231. Power of Two.cpp

---
+++ 231. Power of Two.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<0)
+            return false;
+        int count = 0;
+        while(n){
+            if(n&1==1){
+                count++;
+            }
+            n = n>>1;
+        }
+        if(count==1)
+            return true;
+        return false;
+    }
+};
