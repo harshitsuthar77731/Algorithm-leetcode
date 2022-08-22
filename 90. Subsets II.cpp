251134a83d1e5b4de6f831827bb0f1325c2f62a5
Harshit Suthar <65647274+harshitsuthar77731@users.noreply.github.com>
2022-08-22 09:23:03 +0530

    Create 90. Subsets II.cpp

---
+++ 90. Subsets II.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void fun(int ind,vector<int>&temp,vector<int>& nums,vector<vector<int>>&ans){
+        if(ind == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        // recursive case
+        temp.push_back(nums[ind]);
+        fun(ind+1,temp,nums,ans);
+        temp.pop_back();
+        // imp condn;
+        while(ind+1<nums.size()&&nums[ind]==nums[ind+1]) ind++;
+        fun(ind+1,temp,nums,ans);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        fun(0,temp,nums,ans);
+        return ans;
+    }
+};
